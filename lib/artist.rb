df65a493bb1f834655a3e28f6301413291f3babd
kennja05 <jacobkenny05@gmail.com>
2019-12-13 17:22:15 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,33 @@
+require 'pry'
+class Artist 
+
+    attr_reader :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end 
+
+    def self.all
+        @@all
+    end 
+
+    def songs
+        Song.all.select do |song|
+            song.artist == self 
+        end 
+    end 
+
+    def new_song(name, genre)
+        Song.new(name, self, genre)
+    end 
+
+    def genres
+        songs.map do |song|
+            #binding.pry 
+            song.genre
+        end 
+    end 
+
+end 
